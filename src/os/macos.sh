2d6156a20290baf04a4f5b6f1fb47701209d9ed0
Gustavo Cabezal <meisterveda@gmail.com>
2026-03-15 00:53:57 +0000

    feat(dotfiles): initialize repo from valhalla

--- src/os/macos.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Install command-line tools using Homebrew.
-
-# Make sure we’re using the latest Homebrew.
-brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade
